5b466867d04c685b41ccbc459f5df95b439975a3
proudfb <bproudft@gmail.com>
2016-02-21 21:51:46 -0500

    Initial Commit

---
+++ BasicRepository/DAL/IElementRepository.cs
@@ -0,0 +1,19 @@
+﻿using BasicRepository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicRepository.DAL
+{
+    public interface IElementRepository : IDisposable
+    {
+        IEnumerable<Element> SelectAll();
+        IEnumerable<Element> SelectEachMember(AppEnum.Family family);
+        Element SelectByAtomicNumber(int atomicNumber);
+        void Insert(Element element);
+        void Update(Element element);
+        void Delete(int atomicNumber);
+    }
+}
---
+++ BasicRepository/Global.asax.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace BasicRepository
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            BasicRepository.DAL.Data.InitializeElements();
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+            // event is raised when a session is abandoned or expires
+        }
+    }
+}
